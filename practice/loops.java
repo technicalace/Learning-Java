2b0907e09488a824d4b9c328ce972bbe2309af21
technicalace <80608826+technicalace@users.noreply.github.com>
2021-10-31 13:44:06 -0500

    loops

---
+++ practice/loops.java
@@ -0,0 +1,14 @@
+package practice;
+
+public class loops {
+    public static void main(String[] args) {
+        int x = 0; 
+        x++;
+        while (x < 10) {
+            System.out.println("The output of x is less than 10");
+            if (x > 10) {
+                System.out.println("The output of x is less than 10");
+            }
+        }
+    }
+}
