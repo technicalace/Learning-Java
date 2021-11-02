bd5f7e1e2f4911355d90e7e275e3fd73fa5449a6
technicalace <80608826+technicalace@users.noreply.github.com>
2021-11-01 23:48:27 -0500

    merge big

--- practice/root.java
+++
@@ -1,8 +0,0 @@
-package practice;
-
-public class root {
-    public static void main(String[] args) {
-        int x = 225;
-        System.out.println("The square root of " + x + " is " + Math.sqrt(x));
-    }
-}
