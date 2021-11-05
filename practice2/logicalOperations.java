67272a0528a3416a148badf7a4c5b7f7c0c99872
technicalace <80608826+technicalace@users.noreply.github.com>
2021-11-05 00:27:57 -0500

    major commit

--- practice2/logicalOperations.java
+++ practice2/logicalOperations.java
@@ -1,5 +1,7 @@
 package practice2;
 
+import java.util.Scanner;
+
 public class logicalOperations {
     public static void main(String[] args) {
         // logical operaters = used to connect two or more expressions
@@ -8,7 +10,7 @@ public class logicalOperations {
         //                          || = (OR) either condition must be true
         //                          ! = (NOT) reverses boolean value of condition
 
-        int temp = 15; 
+        /*int temp = 15; 
 
         if(temp > 30) {
             System.out.println("It is hot outside");
@@ -18,6 +20,19 @@ public class logicalOperations {
         }
         else {
             System.out.println("It is cold outside");
+        }*/
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("You are playing a game! Press q or Q to quit");
+        String response = scanner.next();
+
+        if(response.equals("q") || response.equals("Q")) {
+            System.out.println("You quit the game");
         }
+        else {
+            System.out.println("You are still playing the game");
+        }
+
     }
 }
