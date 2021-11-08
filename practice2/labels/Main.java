5c7ec40db419ace32790c6da443fa94646d447bc
technicalace <80608826+technicalace@users.noreply.github.com>
2021-11-08 17:21:01 -0600

    please wrk

--- practice2/labels/Main.java
+++ practice2/labels/Main.java
@@ -8,10 +8,12 @@ public class Main {
     public static void main(String[] args) {
         // JLabel = a GUI display area for a string of text, an image, or both
 
-        ImageIcon image = new ImageIcon();
+        ImageIcon image = new ImageIcon("elmo.jpg");
 
         JLabel label = new JLabel(); // create a label 
         label.setText("Bro do you even code?"); // set text of label 
+        label.setIcon(image); 
+
 
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
