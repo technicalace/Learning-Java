57a8f853ed9ccf67f6634f2059418fb8f0f78c6f
technicalace <80608826+technicalace@users.noreply.github.com>
2021-11-08 17:31:23 -0600

    e

--- practice2/labels/Main.java
+++ practice2/labels/Main.java
@@ -8,11 +8,14 @@ public class Main {
     public static void main(String[] args) {
         // JLabel = a GUI display area for a string of text, an image, or both
 
-        ImageIcon image = new ImageIcon("elmo.jpg");
+        ImageIcon image = new ImageIcon("elmo.png");
 
         JLabel label = new JLabel(); // create a label 
+       
         label.setText("Bro do you even code?"); // set text of label 
         label.setIcon(image); 
+        label.setHorizontalTextPosition(JLabel.CENTER); // set text LEFT, CENTER, RIGHT of imageicon
+        label.setVerticalTextPosition(JLabel.TOP); // set text, TOP, CENTER, BOTTOM of imageicon
 
 
         JFrame frame = new JFrame();
