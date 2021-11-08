2b0192a14d86d79176bd27372e532cc52125004f
technicalace <80608826+technicalace@users.noreply.github.com>
2021-11-08 17:17:02 -0600

    image

---
+++ practice2/labels/Main.java
@@ -0,0 +1,22 @@
+package practice2.labels;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Main {
+    public static void main(String[] args) {
+        // JLabel = a GUI display area for a string of text, an image, or both
+
+        ImageIcon image = new ImageIcon();
+
+        JLabel label = new JLabel(); // create a label 
+        label.setText("Bro do you even code?"); // set text of label 
+
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500,500);
+        frame.setVisible(true);
+        frame.add(label);
+    }   
+}
