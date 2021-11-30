f5bd62441405544ce99eb11e6a184ea467bc6570
technicalace <80608826+technicalace@users.noreply.github.com>
2021-11-29 23:55:06 -0600

    commit

--- projects/firstgame/rpg.java
+++ projects/firstgame/rpg.java
@@ -338,3 +338,37 @@ public class rpg {
    
     
 }
+
+ class dice {
+    
+    public static int roll6() {
+        Random rand = new Random();
+        int n = rand.nextInt(7); 
+        while (n == 0) {
+            n = rand.nextInt(7); 
+
+        } // 1-6
+        return n;
+    }
+
+    public static int roll10() {
+        Random rand = new Random();
+        int n = rand.nextInt(11);
+        while (n == 0) {
+            n = rand.nextInt(11);
+
+        }// 1-10
+        return n;
+    }
+
+    public static int roll20() {
+        Random rand = new Random();
+        int n = rand.nextInt(21);
+        while (n == 0) {
+            n = rand.nextInt(21);
+
+        }// 1-20
+        return n;
+    }
+
+}
